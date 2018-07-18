a8ab30163821096adf3229717d77000aaf2539e9
trucnx2 <dzteam1122@gmail.com>
2018-07-18 23:45:17 +0700

    binhdz

--- app/src/main/java/smartcompass/teamdz/com/smartcompass2018/MainActivity.java
+++ app/src/main/java/smartcompass/teamdz/com/smartcompass2018/MainActivity.java
@@ -1,13 +1,29 @@
 package smartcompass.teamdz.com.smartcompass2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMaps=findViewById(R.id.btn_maps);
+        btnMaps.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_maps:
+                Intent intent = new Intent(MainActivity.this,MapsActivity.class);
+                startActivity(intent);
+        }
     }
 }
