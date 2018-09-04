576750cf90da16a91f4c41aad111bc3958f683b9
nghiaxlh <lenghiax@gmail.com>
2018-09-04 22:03:50 +0700

    add line in map

---
+++ app/src/main/java/smartcompass/teamdz/com/smartcompass2018/view/LineView.java
@@ -0,0 +1,50 @@
+package smartcompass.teamdz.com.smartcompass2018.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Build;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class LineView extends View {
+
+    private float mWidth, mHeight;
+    public LineView(Context context) {
+        super(context);
+    }
+
+    public LineView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LineView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        mWidth = (float) MeasureSpec.getSize(widthMeasureSpec);
+        mHeight = (float) MeasureSpec.getSize(heightMeasureSpec);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.parseColor("#696969"));
+        paint.setStrokeWidth(2.0f);
+        canvas.drawLine(mWidth/2, 0.0f, mWidth/2, mHeight, paint);
+        canvas.drawLine(0.0f, mHeight/2, mWidth, mHeight/2, paint);
+    }
+}
