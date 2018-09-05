44df0d2a68b598e9c682684998ccbc138552b7d0
trucnx2 <dzteam1122@gmail.com>
2018-09-05 22:54:08 +0700

    binhdz

---
+++ app/src/main/java/smartcompass/teamdz/com/smartcompass2018/utils/InterstitialUtils.java
@@ -0,0 +1,77 @@
+package smartcompass.teamdz.com.smartcompass2018.utils;
+
+import android.content.Context;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+
+public class InterstitialUtils {
+    private final String APP_ID="ca-app-pub-9569615767688214~5977359766";
+    private final String INTERSTITIAL_AD_UNIT="ca-app-pub-9569615767688214/1204964831";
+    private InterstitialAd interstitialAd;
+    private static InterstitialUtils sharedInstance;
+    private AdCloseListener adCloseListener;
+    private boolean isReloaded = false;
+
+    public static InterstitialUtils getSharedInstance(){
+        if (sharedInstance==null){
+            sharedInstance=new InterstitialUtils();
+        }
+        return sharedInstance;
+    }
+
+    public void init(Context context){
+        MobileAds.initialize(context,APP_ID);
+        interstitialAd=new InterstitialAd(context);
+        interstitialAd.setAdUnitId(INTERSTITIAL_AD_UNIT);
+        interstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                if(adCloseListener!=null){
+                    adCloseListener.onAdClosed();
+                }
+                loadInterstitialAd();
+            }
+
+            @Override
+            public void onAdFailedToLoad(int i) {
+                super.onAdFailedToLoad(i);
+                if (isReloaded==false){
+                    isReloaded=true;
+                    loadInterstitialAd();
+                }
+            }
+        });
+        loadInterstitialAd();
+    }
+
+    private void loadInterstitialAd() {
+        if (interstitialAd!=null&&!interstitialAd.isLoading()&&!interstitialAd.isLoaded()){
+            AdRequest adRequest = new AdRequest.Builder().build();
+            interstitialAd.loadAd(adRequest);
+        }
+    }
+    public interface AdCloseListener{
+        void onAdClosed();
+    }
+
+    public void  showInterstitialAd(AdCloseListener adCloseListener){
+        if (canShowInterstitialAd()) {
+            isReloaded=false;
+            this.adCloseListener=adCloseListener;
+            interstitialAd.show();
+
+        }else {
+            loadInterstitialAd();
+            adCloseListener.onAdClosed();
+        }
+    }
+
+    private boolean canShowInterstitialAd() {
+        return interstitialAd!=null&&interstitialAd.isLoaded();
+    }
+}
+
